dec4ffecdd66471ccddbc84c47d39d227290e9d3
Gabriel Borges <gborges0727@gmail.com>
2017-02-21 21:39:44 -0500

    Add hash & salt to user passwords in user.js

--- Backend/node-express-gen/user.js
+++ Backend/node-express-gen/user.js
@@ -5,6 +5,7 @@ var passportLocalMongoose = require('passport-local-mongoose');
 var User = new Schema({
 	username: String,
 	password: String,
+	salt: String,
 	admin: {
 	   type: Boolean,
 	   default: false
@@ -15,6 +16,30 @@ var User = new Schema({
 	}, 
 	{ timestamps: true }
 });
+
+User.pre(save, function(next) {
+	var user = this;
+	
+	// Only hash if pass is new / modified
+	if (!user.isModified('password')) return next();
+	
+	// Generate salt
+	bcrypt.genSalt(10, function(err, salt) {
+		if (err) return next(err);
+		
+		// Override the cleartext password with hashed one :) 
+		user.password = hash;
+		next();
+	});
+});
+
+// Use the below to test a user inputed password for authentication! 
+User.methods.comparePassword = function(input, cb) {
+	bcrypt.compare(input, this.password, function(err, isMatch) {
+		if (err) return cb(err);
+		cb(null, isMatch);
+	});
+}
 
 User.plugin(passportLocalMongoose);
 
