67e5926480f94ef614dab73fce9e77e4e1ff0302
Gabriel Borges <gborges0727@gmail.com>
2017-02-22 17:20:25 -0500

    Fix bug

--- Backend/node-express-gen/routes/verify.js
+++ Backend/node-express-gen/routes/verify.js
@@ -1,4 +1,4 @@
-var User = require('../models/user');
+var User = require('../user');
 var jwt = require('jsonwebtoken'); // used to create, sign and verify tokens
 var config = require('../config.js');
 
