c7a11fd63e82c17557aaf87d4c21723f76742422
root <root@plantpal.uconn.edu>
2017-03-01 01:39:41 -0500

    remove https bs

--- Backend/node-express-gen/app.js
+++ Backend/node-express-gen/app.js
@@ -10,6 +10,14 @@ var users = require('./routes/users');
 
 var app = express();
 
+/*// Secure traffic only
+app.all('*', function(req, res, next){
+   if (req.secure) {
+	return next();
+   };
+   res.redirect('https://'+req.hostname+':'+app.get('secPort')+req.url);
+});
+*/
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
