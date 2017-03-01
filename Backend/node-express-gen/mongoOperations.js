9edcdd90457be32a63e7b4108a2895444ae06e8a
Gabriel Borges <gborges0727@gmail.com>
2017-03-01 02:35:17 -0500

    Update mongo operations

--- Backend/node-express-gen/mongoOperations.js
+++ Backend/node-express-gen/mongoOperations.js
@@ -1,15 +1,20 @@
 var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost:27017/plantpal';
 
 exports.insertDocument = function(db, document, collection, callback) {
     // Get the documents collection
-    var coll = db.collection(collection);
-    // Insert some documents
-    coll.insert(document, function(err, result) {
-        assert.equal(err, null);
-        console.log("Inserted " + result.result.n + " documents into the document collection " +
-            collection);
-        callback(result);
-    });
+    MongoClient.connect(url, function (err, db) {
+        var coll = db.collection(collection);
+        // Insert some documents
+        coll.insert(document, function(err, result) {
+            assert.equal(err, null);
+            console.log("Inserted " + result.result.n + " documents into the document collection " +
+                collection);
+            callback(result);
+        });
+    })
 };
 
 exports.findDocuments = function(db, collection, callback) {
