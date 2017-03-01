451e8a124de802a9fe4400112c003276e1f018c0
Gabriel Borges <gborges0727@gmail.com>
2017-03-01 01:59:10 -0500

    update user route

--- Backend/node-express-gen/routes/users.js
+++ Backend/node-express-gen/routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var user = require('./models/userModel')
+var operations = require('./mongoOperations')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
