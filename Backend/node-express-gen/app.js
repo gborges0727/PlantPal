b9008c23f7f7dd372a8ede72402307d9d188d902
root <root@plantpal.uconn.edu>
2017-03-01 01:56:46 -0500

    update app.js with requirements

--- Backend/node-express-gen/app.js
+++ Backend/node-express-gen/app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
 var index = require('./routes/index');
 var users = require('./routes/users');
