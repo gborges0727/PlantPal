0d202c55646183cb48be7906fd517f9e5147e436
Gabriel Borges <gborges0727@gmail.com>
2017-03-01 01:55:34 -0500

    Add post route skeleton in users.js

--- Backend/node-express-gen/routes/users.js
+++ Backend/node-express-gen/routes/users.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/login/', function(req, res, next) {
+    
+});
+
+router.post('/register/', function(req, res, next) {
+    
+});
+
 module.exports = router;
