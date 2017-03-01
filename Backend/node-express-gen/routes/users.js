c831206381585345e080056411208141a19e9b62
Gabriel Borges <gborges0727@gmail.com>
2017-03-01 02:18:06 -0500

    Fix bug

--- Backend/node-express-gen/routes/users.js
+++ Backend/node-express-gen/routes/users.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var user = require('./models/userModel')
+var user = require('../models/userModel')
 var operations = require('./mongoOperations')
 
 /* GET users listing. */
