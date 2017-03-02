cc92965b21149e1f961f84e166e74f4e26a68905
Gabriel Borges <gborges0727@gmail.com>
2017-03-02 18:32:43 -0500

    Fill in loginUser function

--- Backend/node-express-gen/mongoOperations.js
+++ Backend/node-express-gen/mongoOperations.js
@@ -3,6 +3,9 @@ var MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 var Models = require('./models/models');
 var bcrypt = require('bcrypt');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var bcrypt = require('bcrypt');
 
 exports.createUser = function(document, callback) {
     // Get the documents
@@ -13,11 +16,11 @@ exports.createUser = function(document, callback) {
         password: document["password"],
         email: document["email"]
     });
-    
+
     newUser.hashPassword(function(err) {
         if (err) throw err;
     });
-    
+
     // TODO: Add functionality here in order to check if the attempted creation
     // already exists. If so, return the proper response back to the client. If not, 
     // return an ok response to the client. 
@@ -29,11 +32,39 @@ exports.createUser = function(document, callback) {
 };
 
 exports.loginUser = function(document, callback) {
-    User.find({
-        username: document["username"]
-    }, function(err, user) {
-        if (err) throw err;
-        console.log("User found!");
-    });
     //TODO: Functionality here to authenticate the users using whatever means we want.
+    var username = document["username"];
+    var password = document["password"];
+    
+    passport.use(new LocalStrategy(
+        function(username, password) {
+            User.findOne({
+                username: username
+            }, function(err, user) {
+                if (err) {
+                    return callback(err);
+                }
+                if (!user) {
+                    return callback(null, false, {
+                        message: 'Incorrect username.'
+                    });
+                }
+                
+                // Checks password using bcrypt
+                bcrypt.compareSync(passAttempt, password, function(err, result) {
+                    if (err) return callback(err);
+                    if (result === false) {
+                        return callback(null, false, {
+                            message: 'Incorrect password.'
+                        });
+                    } else {
+                        console.log("Sign in successful");
+                        return callback(null, user, {
+                            message: 'User signed in successfully'
+                        });
+                    }
+                });
+            });
+        }
+    ));
 };
