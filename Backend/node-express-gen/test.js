88732cbe8ea23c9a1129868c05db92eef77cd04b
root <root@plantpal.uconn.edu>
2017-03-02 20:03:37 -0500

    https bullshit

---
+++ Backend/node-express-gen/test.js
@@ -0,0 +1,13 @@
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+
+   key: fs.readFileSync('key.pem'),
+   cert:fs.readFileSync('cert.pem')};
+
+var a = https.createServer(options, function(req, res) {
+   res.writeHead(200);
+   res.end("heelo world \n");
+}).listen(4607);
+
