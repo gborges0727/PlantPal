7f2ccb02e361955ed5519a9f0b85771fdd2563d1
Gabriel Borges <gborges0727@gmail.com>
2017-03-27 21:55:10 -0400

    Add feed.js

--- Backend/node-express-gen/routes/feed.js
+++ Backend/node-express-gen/routes/feed.js
@@ -1,6 +1,12 @@
+var fs = require('fs');
+var express = require('express');
+var router = express.Router();
+var model = require('../models/models');
+var mongoose = require('mongoose');
 
-
-
+router.get('/feed', function(res, res, next) {
+    
+});
 
 function getNewestFile(dir, files, callback) {
     if (!callback) return;
