49e8c1e690560d90b13b1a6372ed02f1a98ccc5b
Gabriel Borges <gborges0727@gmail.com>
2017-03-29 00:53:41 -0400

    Beautify code

--- Backend/node-express-gen/routes/myPlants.js
+++ Backend/node-express-gen/routes/myPlants.js
@@ -4,37 +4,40 @@ var router = express.Router();
 var model = require('../models/models');
 var mongoose = require('mongoose');
 
-router.get('/allPlants', function(req, res, next) { 
-    model.User.findOne({username: req.body}, function(err, user){
-	console.log(JSON.stringify('Got user: ' + req.body))
-	if (err) throw err;
-	else
-	console.log('I GOT A USER!!!')
-    })	
+router.get('/allPlants', function(req, res, next) {
+    model.User.findOne({
+        username: req.body
+    }, function(err, user) {
+        console.log(JSON.stringify('Got user: ' + req.body))
+        if (err) throw err;
+        else
+            console.log('I GOT A USER!!!')
+    })
 });
 
 router.get('/specificPlant', function(req, res, next) {
-    model.Flower.findOne({name: req.body}, function(err, flower){
+    model.Flower.findOne({
+        name: req.body
+    }, function(err, flower) {
         if (err) {
             res.writeHead(404, {
                 'Content-Type': 'text/plain'
             });
             res.end('Flower was not found! Please try again');
         }
-        
+
         var flowerToSend = JSON.stringify {
             name: flower.name,
-            sciName: flower.scientificName, 
-            family: flower.family, 
-            nativeRegion: flower.nativeRegion, 
+            sciName: flower.scientificName,
+            family: flower.family,
+            nativeRegion: flower.nativeRegion,
             Description: flower.Description
         }
-        
+
         res.writeHead(200, {
             'Content-Type': 'application/json'
         });
-        
+
         res.end(flowerToSend);
     });
 });
-    
