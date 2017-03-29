4b80db58e7b761ce7d28b1946eb5b390e5b211dd
Gabriel Borges <gborges0727@gmail.com>
2017-03-29 14:05:48 -0400

    fix syntax

--- Backend/node-express-gen/ignoreThisTestingPythonScript.js
+++ Backend/node-express-gen/ignoreThisTestingPythonScript.js
@@ -4,7 +4,7 @@ var pythonshell = require('python-shell');
 pythonshell.defaultOptions = { scriptPath: '/var/www/plantpal.uconn.edu/ProjectFiles/RecogAlgorithms/plant_classification/'
 };
 
-var options = {args: ['-p', '/var/www/plantpal.uconn.edu/ProjectFiles/Backend/node-express-gen/userImages/' + newName + '.jpg']}
+var options = {args: ['-p', '/var/www/plantpal.uconn.edu/ProjectFiles/Backend/node-express-gen/userImages/Hk5lUjO2g.jpg']}
 var pyshell = new pythonshell('classify2.py', options);
 
 pyshell.on('message', function(message) {
