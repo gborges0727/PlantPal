7e8ecbe830345c8de05ba59bf3c91871ac72f5a4
root <root@plantpal.uconn.edu>
2017-03-29 00:44:13 -0400

    add test for myPlants

--- Backend/node-express-gen/routes/myPlants.js
+++ Backend/node-express-gen/routes/myPlants.js
@@ -4,8 +4,13 @@ var router = express.Router();
 var model = require('../models/models');
 var mongoose = require('mongoose');
 
-router.get('/allPlants', function(req, res, next) {
-    
+router.get('/allPlants', function(req, res, next) { 
+    model.User.findOne({username: req.body}, function(err, user){
+	console.log(JSON.stringify('Got user: ' + req.body))
+	if (err) throw err;
+	else
+	console.log('I GOT A USER!!!')
+    })	
 });
 
 router.get('/specificPlant', function(req, res, next) {
