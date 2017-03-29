e3bd728b21b5d9aa40cae1663c6bb6f30a154add
Gabriel Borges <gborges0727@gmail.com>
2017-03-29 16:53:23 -0400

    fix

--- Backend/node-express-gen/ignoreThisTestingPythonScript.js
+++ Backend/node-express-gen/ignoreThisTestingPythonScript.js
@@ -7,7 +7,7 @@ var options = {
     args: ['-p', '/var/www/plantpal.uconn.edu/ProjectFiles/RecogAlgorithms/plant_classification/images/plants/daisy2.jpg'],
     scriptPath: '/var/www/plantpal.uconn.edu/ProjectFiles/RecogAlgorithms/plant_classification/'
 };
-var pyshell = new pythonshell('myscript.py', options);
+var pyshell = new PythonShell('myscript.py', options);
 
 pyshell.on('message', function(message) {
     // received a message sent from the Python script
