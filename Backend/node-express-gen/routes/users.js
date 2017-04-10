76fcf7a7c59aede855ee3d20bb8e5da5a8051901
Gabriel Borges <gborges0727@gmail.com>
2017-04-10 16:44:38 -0400

    Add code from pythontest script to the route

--- Backend/node-express-gen/routes/users.js
+++ Backend/node-express-gen/routes/users.js
@@ -40,19 +40,12 @@ router.post('/upload/', function(req, res) {
 
     form.on('field', function(fieldName, textValue) {        
         // Below code runs the analysis
-        console.log(newName);
-        var options = {args: ['-p', '/var/www/plantpal.uconn.edu/ProjectFiles/Backend/node-express-gen/userImages/' + newName + '.jpg']}
-        var pyshell = new pythonshell('classify2.py', options);
-        
-        pyshell.on('message', function(message) {
-            // received a message sent from the Python script (a simple "print" statement)
-            console.log(message);
-            plantName = message;
-        });
+        var cmd = 'python -W ignore /var/www/plantpal.uconn.edu/ProjectFiles/RecogAlgorithms/plant_classification/classify2.py -p /var/www/plantpal.uconn.edu/ProjectFiles/Backend/node-express-gen/userImages/' + newName + '.jpg';
 
-        pyshell.end(function(err) {
-            if (err) throw err;
+        exec(cmd, function(error, stdout, stderr) {
+            if (stderr) console.log(stderr);
             console.log('Picture analysis complete');
+            plantName = stdout;
             model.User.findOneAndUpdate({
                     username: textValue
                 }, {
