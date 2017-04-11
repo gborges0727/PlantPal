02907a732c12beb23202486ca10297d9bb938de0
Gabriel Borges <gborges0727@gmail.com>
2017-04-11 18:11:30 -0400

    test fix

--- Backend/node-express-gen/routes/myPlants.js
+++ Backend/node-express-gen/routes/myPlants.js
@@ -17,11 +17,11 @@ router.get('/allPlants', function(req, res, next) {
             res.end('Error: Username was not found! Please try again');
         }
         
-        var userPictures = JSON.stringify {
+        var userPictures = JSON.stringify (
             pictures: [{
                 user.pictures
             }]
-        };
+        );
         
         res.writeHead(200, {
             'Content-Type': 'application/json'
