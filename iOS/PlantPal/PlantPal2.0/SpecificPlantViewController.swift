0626a820d1efbc1cc1a8ede9f0ff122a397bbd6c
Gabriel Borges <gborges0727@gmail.com>
2017-04-11 20:37:03 -0400

    Add new viewcontroller for specific image viewing

---
+++ iOS/PlantPal/PlantPal2.0/SpecificPlantViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SpecificPlantViewController.swift
+//  PlantPal2.0
+//
+//  Created by Gabriel Borges on 4/11/17.
+//  Copyright © 2017 Gabe Borges. All rights reserved.
+//
+
+import UIKit
+
+class SpecificPlantViewController: UIViewController {
+    
+}
