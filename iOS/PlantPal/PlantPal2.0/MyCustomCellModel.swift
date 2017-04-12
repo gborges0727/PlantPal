8c9ec9f5636815f834d46d9b3111477f583a52bd
Gabriel Borges <gborges0727@gmail.com>
2017-04-12 11:08:25 -0400

    Create class for custom cell

---
+++ iOS/PlantPal/PlantPal2.0/MyCustomCellModel.swift
@@ -0,0 +1,14 @@
+//
+//  MyCustomCellModel.swift
+//  PlantPal2.0
+//
+//  Created by Gabriel Borges on 4/12/17.
+//  Copyright © 2017 Gabe Borges. All rights reserved.
+//
+
+import UIKit
+class MyCustomCellModel: UITableViewCell {
+    
+    
+    
+}
