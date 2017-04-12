f98783e82f79d6f9f7a672e8f5fdb605339d0e6c
Gabriel Borges <gborges0727@gmail.com>
2017-04-11 20:40:31 -0400

    Link button back to the tabbar

--- iOS/PlantPal/PlantPal2.0/SpecificPlantViewController.swift
+++ iOS/PlantPal/PlantPal2.0/SpecificPlantViewController.swift
@@ -10,4 +10,9 @@ import UIKit
 
 class SpecificPlantViewController: UIViewController {
     
+    @IBAction func backToAllPlants(_ sender: UIButton) {
+        let tabBarView = self.storyboard?.instantiateViewController(
+            withIdentifier: "TabBar") as! TabBarViewController
+        self.present(tabBarView, animated: true, completion: nil)
+    }
 }
