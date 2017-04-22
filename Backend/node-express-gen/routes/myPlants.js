de234cbd9f841caebf7e3b2863a7a7a3f0d3264e
Gabriel Borges <gborges0727@gmail.com>
2017-04-22 19:02:34 -0400

    add functionality that makes sure no elements are null

--- Backend/node-express-gen/routes/myPlants.js
+++ Backend/node-express-gen/routes/myPlants.js
@@ -16,15 +16,15 @@ router.post('/allPlants', function(req, res, next) {
             });
             res.end('Error: Username was not found! Please try again');
         }
-        
-        var userPictures = JSON.stringify ({
+
+        var userPictures = JSON.stringify({
             pictures: user.pictures
         });
-        
+
         res.writeHead(200, {
             'Content-Type': 'application/json'
         });
-        
+
         res.end(userPictures);
     });
 });
@@ -39,10 +39,9 @@ router.post('/specificPlant', function(req, res, next) {
                 'Content-Type': 'text/plain'
             });
             res.end('Flower was not found! Please try again');
-        }
-        else {
+        } else {
             console.log(flower.Name);
-            var flowerToSend = JSON.stringify ({
+            var flowerToSend = JSON.stringify({
                 name: flower.Name,
                 sciName: flower.ScientificName,
                 family: flower.Family,
@@ -60,45 +59,45 @@ router.post('/specificPlant', function(req, res, next) {
 });
 
 router.post('/uploadPlant', function(req, res, next) {
-    if(!req.body["name"] || !req.body["scientificName"], || !req.body["family"] 
-            || req.body["nativeRegion"] || !Description: req.body["description"]) {
+    if (!req.body["name"] || !req.body["scientificName"], || !req.body["family"] ||
+        req.body["nativeRegion"] || !Description: req.body["description"]) {
+        res.writeHead(401, {
+            'Content-Type': 'text/plain'
+        });
+        res.end('Missing flower elements!');
+    } else {
+        var newPlant = new model.Flower({
+            Name: req.body["name"],
+            ScientificName: req.body["scientificName"],
+            Family: req.body["family"],
+            NativeRegion: req.body["nativeRegion"],
+            Description: req.body["description"]
+        });
+
+        model.Flower.findOne({
+            Name: req.body["name"]
+        }, function(err, flower) {
+            if (err) throw err;
+            else if (!flower) {
+                newPlant.save(function(err, result) {
+                    if (err) {
+                        throw err;
+                    } else {
+                        console.log("Flower created successfully!");
+                        res.writeHead(200, {
+                            'Content-Type': 'text/plain'
+                        });
+                        res.end('Flower created successfully!');
+                    }
+                });
+            } else {
                 res.writeHead(401, {
                     'Content-Type': 'text/plain'
                 });
-                res.end('Missing flower elements!');
+                res.end('Flower already exists!');
+            }
+        });
     }
-        
-    var newPlant = new model.Flower({
-        Name: req.body["name"], 
-        ScientificName: req.body["scientificName"], 
-        Family: req.body["family"], 
-        NativeRegion: req.body["nativeRegion"], 
-        Description: req.body["description"]
-    });
-    
-    model.Flower.findOne({
-        Name: req.body["name"]
-    }, function(err, flower) {
-        if (err) throw err;
-        else if (!flower) {
-            newPlant.save(function(err, result) {
-                if (err) {
-                    throw err;
-                } else {
-                    console.log("Flower created successfully!");
-                    res.writeHead(200, {
-                        'Content-Type': 'text/plain'
-                    });
-                    res.end('Flower created successfully!');
-                }
-            });
-        } else {
-            res.writeHead(401, {
-                'Content-Type': 'text/plain'
-            });
-            res.end('Flower already exists!');
-        }
-    });
 });
 
 module.exports = router;
