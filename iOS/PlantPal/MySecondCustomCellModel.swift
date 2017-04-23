df36b8c4cbe44881aca4233162119cf844d6f24e
Gabriel Borges <gborges0727@gmail.com>
2017-04-22 20:28:15 -0400

    create second custom cell

---
+++ iOS/PlantPal/MySecondCustomCellModel.swift
@@ -0,0 +1,24 @@
+//
+//  MySecondCustomCellModel.swift
+//  PlantPal2.0
+//
+//  Created by Gabriel Borges on 4/22/17.
+//  Copyright © 2017 Gabe Borges. All rights reserved.
+//
+
+import UIKit
+
+class MySecondCustomCellModel: UITableViewCell {
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var flowerNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var plantPictureView: UIImageView!
+    
+    @IBAction func commentButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func likeButton(_ sender: UIButton) {
+    }
+    
+}
