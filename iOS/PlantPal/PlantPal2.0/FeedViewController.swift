ddb4a59c3d1bec4b9a260f7a11c80f323f026a0a
Gabriel Borges <gborges0727@gmail.com>
2017-04-24 20:19:01 -0400

    BUG PRESENT: index out of range. Committing to share file.

--- iOS/PlantPal/PlantPal2.0/FeedViewController.swift
+++ iOS/PlantPal/PlantPal2.0/FeedViewController.swift
@@ -12,6 +12,7 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
     // plantInfo stores the 10 most recently uploaded pictures that are on the server
     var plantInfo:[String] = []
     let cellReuseIdentifier = "cell2"
+    var plantNameAndUsername:[(String, String)] = [] // First string in tuple is name, second is username
     
     @IBOutlet weak var plantList: UITableView!
     
@@ -36,8 +37,6 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("run")
-        print(self.plantInfo.count)
         return self.plantInfo.count
     }
     
@@ -47,28 +46,79 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
         if let url  = NSURL(string: "https://plantpal.uconn.edu:4607/dispImages" + "/var/www/plantpal.uconn.edu/ProjectFiles/Backend/node-express-gen/userImages/" + self.plantInfo[indexPath.row]),
             let data = NSData(contentsOf: url as URL)
         {
-            print("this was reached")
             cell.plantPictureView.image = UIImage(data: data as Data)
         }
         
-        //cell.plantLabel.text = self.plantInfo[indexPath.row]
+        cell.flowerNameLabel.text = plantNameAndUsername[indexPath.row].0
+        cell.userNameLabel.text = plantNameAndUsername[indexPath.row].1
         
         // TODO: Fill out remainder of plant info cell here once it is realized
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        /*
-        //print("You tapped cell number \(indexPath.row).")
-        let specificPlantView = self.storyboard?.instantiateViewController(
-            withIdentifier: "specificPlant") as! SpecificPlantViewController
-        //specificPlantView.plantInfo = self.plantInfo[indexPath.row]
-        // Make the above pass the correct information once it is realized
-        self.present(specificPlantView, animated: true, completion: nil) */
+        // TODO: Input functionality to move to specific view controller.
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 300.0; // Sets custom row height
+    }
+    
+    // is called at the end of getFeedList()
+    func getNameAndUsername() {
+        self.plantNameAndUsername.removeAll()
+        for plant in plantInfo {
+            do {
+                let url = URL(string: "https://plantpal.uconn.edu:4607/feed/getPlantInfo")
+                var request = URLRequest(url: url!)
+                request.httpMethod = "POST"
+                request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+                
+                // add plant name to body request
+                let infoDictionary = [
+                    "fileName": plant
+                ]
+                let jsonObject = try JSONSerialization.data(withJSONObject: infoDictionary,
+                                                            options: .prettyPrinted)
+                request.httpBody = jsonObject
+                
+                // Send request
+                let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                    let httpResponse = response as! HTTPURLResponse
+                    let statusCode = httpResponse.statusCode
+                    if (statusCode == 200) {
+                        do {
+                            if let data = data,
+                                let jsonResponse = try JSONSerialization.jsonObject(with: data) as? [String: Any] {
+                                if let plantName = jsonResponse["plantName"] as? String,
+                                    let uploader = jsonResponse["uploader"] as? String {
+                                    self.plantNameAndUsername.append((plantName.lowercased().capitalized, uploader))
+                                }
+                            }
+                        } catch {
+                            print("Error deserializing JSON: \(error)")
+                        }
+                    }
+                    else if (statusCode == 404) {
+                        let alertController = UIAlertController(title: "Connection Error!",
+                                                                message: "Unable to retrieve your plant info. Please retry in a few minutes!" as String?, preferredStyle: .alert)
+                        let defaultAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+                        alertController.addAction(defaultAction)
+                        self.present(alertController, animated: true, completion: nil)
+                    } else {
+                        let alertController = UIAlertController(title: "Error",
+                                                                message: "Unknown Error Occured!", preferredStyle: .alert)
+                        let defaultAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+                        alertController.addAction(defaultAction)
+                        self.present(alertController, animated: true, completion: nil)
+                    }
+                }
+                task.resume()
+            } catch {
+                print(error.localizedDescription)
+            }
+            
+        }
     }
     
     func getFeedList() {
@@ -114,6 +164,7 @@ class FeedViewController: UIViewController, UITableViewDataSource, UITableViewDe
         } catch {
             print(error.localizedDescription)
         }
+        getNameAndUsername()
     }    
 }
 
