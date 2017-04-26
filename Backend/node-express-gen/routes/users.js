c8e4fb70e727c299b6af4bfedea4259a1bc4d7d2
Gabriel Borges <gborges0727@gmail.com>
2017-04-26 00:37:08 -0400

    remove newlines

--- Backend/node-express-gen/routes/users.js
+++ Backend/node-express-gen/routes/users.js
@@ -43,9 +43,10 @@ router.post('/upload/', function(req, res) {
         exec(cmd, function(error, stdout, stderr) {
             if (stderr) console.log(stderr);
             console.log('Picture analysis complete');
-            plantsAndPercents = stdout.split(" ");
+            plantInfo = stdout.replace(/^\s+|\s+$/g, '');
+            plantsAndPercents = plantInfo.split(" ");
             console.log(plantsAndPercents);
-            //plantName = plantName.replace(/^\s+|\s+$/g, '');
+            
             model.User.findOneAndUpdate({
                     username: textValue
                 }, {
