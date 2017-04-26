76ff5c654f7ff825c2eab6dac0a99a9c53a253cd
Gabriel Borges <gborges0727@gmail.com>
2017-04-26 00:38:22 -0400

    add comment

--- Backend/node-express-gen/routes/users.js
+++ Backend/node-express-gen/routes/users.js
@@ -43,10 +43,11 @@ router.post('/upload/', function(req, res) {
         exec(cmd, function(error, stdout, stderr) {
             if (stderr) console.log(stderr);
             console.log('Picture analysis complete');
+            
+            // The line below removes the newline character from stdout
             plantInfo = stdout.replace(/^\s+|\s+$/g, '');
             plantsAndPercents = plantInfo.split(" ");
             console.log(plantsAndPercents);
-            
             model.User.findOneAndUpdate({
                     username: textValue
                 }, {
